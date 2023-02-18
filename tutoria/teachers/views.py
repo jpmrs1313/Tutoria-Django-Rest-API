c65caace68f8b7229886dd92d32db8207de42475
jpmrs1313 <jpmrs1313@gmail.com>
2023-02-18 16:37:14 +0000

    add filter documentation to teachers

--- tutoria/teachers/views.py
+++ tutoria/teachers/views.py
@@ -1,16 +1,45 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Teacher
 from .serializers import TeacherSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "first_name",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Filter by first name",
+            ),
+            openapi.Parameter(
+                "last_name",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Filter by last name",
+            ),
+            openapi.Parameter(
+                "number",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Filter by number",
+            ),
+            openapi.Parameter(
+                "email",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Filter by email",
+            ),
+        ]
+    )
     def list(self, request):
-        first_name = self.request.query_params.get("firtst_name", None)
+        first_name = self.request.query_params.get("first_name", None)
         last_name = self.request.query_params.get("last_name", None)
         number = self.request.query_params.get("number", None)
         email = self.request.query_params.get("email", None)
@@ -18,16 +47,16 @@ class TeacherViewSet(viewsets.ModelViewSet):
         teachers = Teacher.objects.all()
 
         if email is not None:
-            email = teachers.filter(user__email__startswith=email)
+            teachers = teachers.filter(user__email=email)
 
         if first_name is not None:
-            teachers = teachers.filter(user__first_name__startswith=first_name)
+            teachers = teachers.filter(user__first_name=first_name)
 
         if last_name is not None:
-            teachers = teachers.filter(user__last_name__startswith=last_name)
+            teachers = teachers.filter(user__last_name=last_name)
 
         if number is not None:
-            teachers = teachers.filter(number__startswith=number)
+            teachers = teachers.filter(number=number)
 
         serializer = TeacherSerializer(teachers, many=True)
         return Response(serializer.data)
